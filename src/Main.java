008e7a19742594d589508c0a1c7b89ab7000a5b0
Finn294 <finnblaschke7@web.de>
2022-06-10 15:49:02 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SuchBild display = new SuchBild();
+        display.setVisible(true);
+
+    }
+}
